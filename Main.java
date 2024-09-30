0f2c102d6b2a31fdddb868d5af90b682f51bd91a
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2024-09-29 21:07:39 -0700

    Main file to ask for user input in dealing with student records, main logic inside StudentRecordManager.java

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+public class Main{
+    public static void main(String []args){
+        StudentRecordManager manager = new StudentRecordManager();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice){
+                case 1:
+                    manager.addStudent();
+                    break;
+                case 2:
+                    manager.viewStudent();
+                    break;
+                case 3:
+                    manager.updateStudents();
+                    break;
+                case 4:
+                    manager.deleteStudents();
+                    break;
+                case 5:
+                    System.out.println("Exiting the program...")
+                   System.exit(0);
+                default:
+                    System.out.println("Invalid option, please try again.")
+            }
+        }
+    }
+}
